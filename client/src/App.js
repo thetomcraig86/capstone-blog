64c0ccf56ad88d9827a6d16651ce4dda876cab9c
thetomcraig86 <66853106+thetomcraig86@users.noreply.github.com>
2021-10-23 17:48:18 -0400

    api/MongoDB conn
    consolidated UI into client folder
    created api folder
    added express dotenv mongoose
    created mongodb conn

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/home/Home";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <NavBar/>
+      <Switch>
+      <Route exact path="/">
+            <Home />
+          </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
