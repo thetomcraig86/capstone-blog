fa95382c2c08dc3a7278c53950d25d51397800df
thetomcraig86 <66853106+thetomcraig86@users.noreply.github.com>
2021-10-25 11:28:47 -0400

    base api setup

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const postsRoute = require("./routes/posts");
 
 dotenv.config();
+app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URL)
     .then(console.log("Connected to MongoDB")).catch(err=>console.log(err));
+
+
+app.use("/api/posts", postsRoute);
 
 
 app.listen("5000", () =>{
---
+++ api/models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+    {
+     title:{
+         type:String,
+         required:true,
+         unique:true
+     },
+     author:{
+        type:String,
+        required:true
+     },
+     content: {
+        type:String,
+        required:true 
+     } 
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("Post", PostSchema);
