c4f58933297edf2ecbf2e918ed0740beb58e08ef
thetomcraig86 <66853106+thetomcraig86@users.noreply.github.com>
2021-11-18 13:19:19 -0500

    reading for build

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const postsRoute = require('./routes/posts');
-const authRoute = require('./routes/auth');
+const postsRoute = require('./api/routes/posts');
+const authRoute = require('./api/routes/auth');
 const multer = require('multer');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use("/images", express.static(path.join(__dirname, "/api/images/")));
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -35,12 +32,18 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
 app.use('/api/posts', postsRoute);
 app.use('/api/auth', authRoute);
 
-app.listen('5000', () => {
-  console.log('Backend is running.');
-});
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('../client/build'))
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-app.listen(PORT, console.log(`Server is starting at ${PORT}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
