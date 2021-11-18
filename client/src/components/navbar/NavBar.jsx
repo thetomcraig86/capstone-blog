5a59984d72441e92d62140ab3d6439dee0c454c3
thetomcraig86 <66853106+thetomcraig86@users.noreply.github.com>
2021-11-17 19:57:03 -0500

    navbar adjustments
    tryed to add mobile friendly navbar, ra into issues, not rendering properly

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -11,56 +11,67 @@ export default function NavBar() {
   };
 
   return (
-    <div className='navbar'>
-      <div className='topLeft'>
-        <i className='topIcon fab fa-facebook-square'></i>
-        <i className='topIcon fab fa-twitter-square'></i>
-        <i className='topIcon fab fa-instagram-square'></i>
+    <nav className='navbar navbar-expand-md bg-dark navbar-dark fixed-top'>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#navbarContent'
+        aria-controls='navbarContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <i className='fas fa-bars hamburger'></i>
+      </button>
+      <div className='collapse navbar-collapse justify-content-between' id='navbarContent'>
+      <div className="icons">
+        <i className='topIcon fab fa-facebook-square nav-item nav-link'></i>
+        <i className='topIcon fab fa-twitter-square nav-item nav-link'></i>
+        <i className='topIcon fab fa-instagram-square nav-item nav-link'></i>
       </div>
-      <div className='topCenter'>
-        <ul className='navList'>
-          <li className='navListItem'>
-            <Link className='link' to='/'>
+
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>
               HOME
             </Link>
           </li>
           {user && (
-            <li className='navListItem'>
-              <Link className='link' to='/write'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/write'>
                 WRITE
               </Link>
             </li>
           )}
-          <li className='navListItem' onClick={handleLogout}>
+          <li className='nav-link' onClick={handleLogout}>
             {user && 'LOGOUT'}
           </li>
         </ul>
-      </div>
-      <div className='topRight'>
+
         {user ? (
-          <Link className='link' to='/write'>
+          <Link className='nav-link' to='/write'>
             <img
-              className='profilePic'
+              className='profilePic nav-item'
               src='https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=47&w=79'
               alt='profile pic'
             />
           </Link>
         ) : (
-          <ul className='navList'>
-            <li className='navListItem'>
-              <Link className='link' to='/login'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/login'>
                 LOGIN
               </Link>
             </li>
-            <li className='navListItem'>
-              <Link className='link' to='/register'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/register'>
                 REGISTER
               </Link>
             </li>
           </ul>
         )}
-        <i className='topIconSearch fas fa-search'></i>
+        <i className='topIconSearch fas fa-search nav-link'></i>
       </div>
-    </div>
+    </nav>
   );
 }
