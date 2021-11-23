44d1823a952bd241793ae3bb8ae60ca1f30ad7d8
thetomcraig86 <66853106+thetomcraig86@users.noreply.github.com>
2021-11-23 12:11:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const PORT = process.env.PORT || 80;
 dotenv.config();
 app.use(express.json());
 
-//This will create a middleware.
-//When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -26,5 +24,13 @@ app.use('/api/posts', postsRoute);
 app.use('/api/auth', authRoute);
 
 
-
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+  }
+  
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
